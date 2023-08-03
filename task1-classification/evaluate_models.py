c2306148ab6af05a65eaaf07f7c38c5dfa6a6d60
Nikunj Chhabra <nikunj.chhabra@yahoo.com>
2023-08-02 20:53:38 -0400

    Fixed evalute_models.py

--- task1-classification/evaluate_models.py
+++ task1-classification/evaluate_models.py
@@ -1,3 +1,8 @@
+# This file contains WordVec, Naive Bayes, Binary Naive Bayes and Logistic regression.
+# The models have been used to make predictions for the task1 - predicting spoilerType
+
+# Run the file as -- python3 evaluate_models.py
+
 import nltk
 import json
 import numpy as np
@@ -12,8 +17,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from keras.utils import to_categorical
 from keras.utils import pad_sequences
-# from tokenizers import Tokenizer
-# import tensorflow as tf
 from keras.preprocessing.text import Tokenizer
 from keras import Sequential
 from tensorflow.keras.layers import Embedding, Conv1D, Dense, GlobalMaxPooling1D
@@ -87,55 +90,56 @@ def init_model(train, test, data):
             test_y.append(t['tags'][0])
     
     print("Using only " + data + " to train the models")
-    ### Word2vec
-    # if(data == 'title'):
-    #     vectorizer = CountVectorizer(ngram_range=(1, 2))
-    # else:
-    #     vectorizer = CountVectorizer(ngram_range=(2, 2))
-    # corpus_features = vectorizer.fit_transform(train_x)
-    # corpus_test_features = vectorizer.transform(test_x)
-    # classifier = MultinomialNB()
-    # classifier.fit(corpus_features, train_y)
-    # pred = classifier.predict(corpus_test_features)
-    # corpus_test_accuracy = accuracy_score(test_y, pred)
-    # print('The accuracy of Word2vec is: ') 
-    # print(str(corpus_test_accuracy))
+
+    ## Word2vec
+    if(data == 'title'):
+        vectorizer = CountVectorizer(ngram_range=(1, 2))
+    else:
+        vectorizer = CountVectorizer(ngram_range=(2, 2))
+    corpus_features = vectorizer.fit_transform(train_x)
+    corpus_test_features = vectorizer.transform(test_x)
+    classifier = MultinomialNB()
+    classifier.fit(corpus_features, train_y)
+    pred = classifier.predict(corpus_test_features)
+    corpus_test_accuracy = accuracy_score(test_y, pred)
+    print('The accuracy of Word2vec is: ') 
+    print(str(corpus_test_accuracy))
     
 
-    # ### Naive Bayes
-    # model2 = make_pipeline(TfidfVectorizer(), MultinomialNB())
-    # model2.fit(train_x, train_y)
-    # spoiler_types_pred = model2.predict(test_x)
-    # print('The accuracy of Naive Bayes is: ') 
-    # print(accuracy_score(test_y, spoiler_types_pred))
+    ### Naive Bayes
+    model2 = make_pipeline(TfidfVectorizer(), MultinomialNB())
+    model2.fit(train_x, train_y)
+    spoiler_types_pred = model2.predict(test_x)
+    print('The accuracy of Naive Bayes is: ') 
+    print(accuracy_score(test_y, spoiler_types_pred))
 
-    ### Binary Naive Bayes
-    # _train_y = []
-    # for y in train_y:
-    #     if y == 'multi':
-    #         _train_y.append('multi')
-    #     else:
-    #         _train_y.append('n')
-    # model1 = make_pipeline(TfidfVectorizer(), MultinomialNB())
-    # model1.fit(train_x, _train_y)
+    ## Binary Naive Bayes
+    _train_y = []
+    for y in train_y:
+        if y == 'multi':
+            _train_y.append('multi')
+        else:
+            _train_y.append('n')
+    model1 = make_pipeline(TfidfVectorizer(), MultinomialNB())
+    model1.fit(train_x, _train_y)
     
-    # model2 = make_pipeline(TfidfVectorizer(), MultinomialNB())
-    # i = 0
-    # while i < len(train_x):
-    #     del train_x[i]
-    #     del train_y[i]
-    #     i+=1
-    # model2.fit(train_x, train_y)
-    # pred = []
-    # for x in test_x:
-    #     p = model1.predict([x])
-    #     if p[0] == 'multi':
-    #         pred.append(p[0])
-    #     else:
-    #         p = model2.predict([x])
-    #         pred.append(p[0])
-    # print('The accuracy of Binary Naive Bayes is: ') 
-    # print(accuracy_score(test_y, pred))
+    model2 = make_pipeline(TfidfVectorizer(), MultinomialNB())
+    i = 0
+    while i < len(train_x):
+        del train_x[i]
+        del train_y[i]
+        i+=1
+    model2.fit(train_x, train_y)
+    pred = []
+    for x in test_x:
+        p = model1.predict([x])
+        if p[0] == 'multi':
+            pred.append(p[0])
+        else:
+            p = model2.predict([x])
+            pred.append(p[0])
+    print('The accuracy of Binary Naive Bayes is: ') 
+    print(accuracy_score(test_y, pred))
 
     ### CNN
     le = LabelEncoder()
@@ -153,6 +157,7 @@ def init_model(train, test, data):
     model3.add(Dense(3, activation='softmax'))
     model3.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     model3.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
+    print("Running CNN model :")
     for i in range(0, len(test_x)):
         new_post = test_x[i]
         new_post_seq = tokenizer.texts_to_sequences([new_post])
@@ -162,24 +167,18 @@ def init_model(train, test, data):
 
 
     ### logic regression
-    # sentences = train_x
-    # y = train_y
-    # vectorizer = CountVectorizer()
-    # # vectorizer = HashingVectorizer(n_features=2**25)
-    # # vectorizer = TfidfVectorizer(sublinear_tf=True, encoding='utf-8', decode_error='ignore', stop_words='english')
-    # X = vectorizer.fit_transform(sentences)
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05, random_state=42)
-    # grid = {"C": np.logspace(-3, 3, 7), "penalty": ["l2"]}
-    # # clf = LogisticRegression(penalty='l2', solver='newton-cg')
-    # logreg = LogisticRegression(penalty='l2',solver='newton-cg')
-    # logreg_cv = GridSearchCV(logreg, grid, cv=10)
-    # logreg_cv.fit(X_train, y_train)
-    # # clf = RidgeClassifierCV(alphas=(0.1, 1.0, 10.0),)
-    # # clf.fit(X_train, y_train)
-    # # print('Accuracy:', clf.score(X_test, y_test))
-    # # print("Tuned hyperparameters: ", logreg_cv.best_params_)
-    # print('The accuracy of Logistic Regression is: ')
-    # print(logreg_cv.best_score_)
+    sentences = train_x
+    y = train_y
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(sentences)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05, random_state=42)
+    grid = {"C": np.logspace(-3, 3, 7), "penalty": ["l2"]}
+    logreg = LogisticRegression(penalty='l2',solver='newton-cg')
+    logreg_cv = GridSearchCV(logreg, grid, cv=10)
+    logreg_cv.fit(X_train, y_train)
+    # print("Tuned hyperparameters: ", logreg_cv.best_params_)
+    print('The accuracy of Logistic Regression is: ')
+    print(logreg_cv.best_score_)
 
 
 def build_model(train, test):
